c9a058e9738b05109e87f0db146349dcdb918992
VeronicaTaucci <taucci.veronica@gmail.com>
2022-02-12 19:22:45 -0500

    created a file part of homework assignment

---
+++ me-and-github.md
@@ -0,0 +1,11 @@
+What is the difference between Git and Github?
+Git is a version control system that lets you manage and keep track of your source code history. GitHub is a cloud-based hosting service that lets you manage Git repositories. If you have open-source projects that use Git, then GitHub is designed to help you better manage them.
+
+Why use Git?
+git is a local VCS software that enables developers to save snapshots of their projects over time. It's generally best for individual use
+
+Why use Github?
+GitHub is a web-based platform that incorporates git's version control features so they can be used collaboratively.
+
+What is the payoff to using Git and Github?
+you can put them to work in order to streamline your development process. You can even integrate the two and use them in conjunction.it makes it easier to resolve errors and fix other mistakes that might occur during development. You can also note changes in each version, to help any team members stay up-to-date on what’s completed and what still needs to be accomplished.
--- python_notes.py
+++ python_notes.py
@@ -2,74 +2,74 @@
 
 
 # ------------------data types-------------
-# !string 
+# !string
 # "any text here"
-#!Integer
+# !Integer
 # a whole number (0, 42, 8675309)
-#! Float
+# ! Float
 # a number with a decimal point (3.14159, 6.626068, 1.21)
-#! Boolean 
+# ! Boolean
 # True or False
-#! List 
+# ! List
 # a sequence of values ([1, 2, 3, 4], ["milk", "butter", "eggs"])
-#! Dictionary 
+# ! Dictionary
 # a sequence of labeled values ({ "name": "Galahad", "quest": "Seek the Grail", "favorite_color": "Blue"})
 
-#! literal
+# ! literal
 # Developers use the term "literal" to refer to a value that is not in a variable. For example, "Hello World" is a String literal, and 42 is a Number literal.
 
-#!Concatenation 
+# !Concatenation
 # #is the process of merging two strings into a single object which is done by using the “+” operator.
 
-#! escape characters:
+# ! escape characters:
 # \b	backspace
 # \t	horizontal tab
 # \v	vertical tab
 # \n	newline
 
-#! Operators
+# ! Operators
 # "+"	Addition
 # "-"	Subtraction
 # "*"	Multiplication
 # "/"	   Division
-# "//"  division    w/o    decimal    
-# "%" modulus/remainder    
+# "//"  division    w/o    decimal
+# "%" modulus/remainder
 # "**"  exponentiation
 
-#! Expressions
+# ! Expressions
 # syntax that return a value
 
-#! String Formatting!!!!!!!!!!!!
-#* By argument: 
+# ! String Formatting!!!!!!!!!!!!
+# * By argument:
 # "Hello {} {}".format(first_name, last_name)
-#* By index
-#"Hello {0} {1}, again {0}".format(first_name, last_name)
-#* By keyword
-#"Hello {first} {last}, again {first}".format(first=first_name, last=last_name)
-#* f string
+# * By index
+# "Hello {0} {1}, again {0}".format(first_name, last_name)
+# * By keyword
+# "Hello {first} {last}, again {first}".format(first=first_name, last=last_name)
+# * f string
 # f'some string {variable}'
-#* older way:
+# * older way:
 # name = "Eric"
 # age = 74
 # "Hello, %s. You are %s." % (name, age)
 # 'Hello Eric. You are 74.'
 
-#! Built in functions---------
+# ! Built in functions---------
 
-#!type() 
-# method returns class type of the argument(object) passed as parameter. 
-#!isinstance() 
+# !type()
+# method returns class type of the argument(object) passed as parameter.
+# !isinstance()
 # function returns True if the specified object is of the specified type, otherwise False.
-#! print()
-#causes output to appear on your terminal screen.
-#! input()
+# ! print()
+# causes output to appear on your terminal screen.
+# ! input()
 # waits for the user to type some input and press return. It then gets whatever was typed.
-#! int()
-#Converts a string to a number
+# ! int()
+# Converts a string to a number
 
-#! conditions-------------------
+# ! conditions-------------------
 
-#* sintax for if, elif, els:
+# * sintax for if, elif, els:
 # if condition :
 # write statement here
 # elif condition 2:
@@ -77,9 +77,9 @@
 # else:
 # write statement here
 
-# x = 5         --> sets variable  x  to  5    
-# x += 3     --> increment    x    by  3    
-# x -= 2   --> decrement    x    by    2    
+# x = 5         --> sets variable  x  to  5
+# x += 3     --> increment    x    by  3
+# x -= 2   --> decrement    x    by    2
 # x *= 5  -->   x = x * 5
 # x /= 5  -->   x = x / 5
 # x %= 5  -->   x = x % 5 returns the remainder when the first operand is divided by the second
@@ -91,33 +91,53 @@
 # x >>= 5  -->   x = x >> 5 hift right by pushing copies of the leftmost bit in from the left, and let the rightmost bits fall off
 # x <<= 5  --> Shift left by pushing zeros in from the right and let the leftmost bits fall off
 
-#! Sequence?
-#In Python, a sequence is a data type that can store multiple, individual values
+# ! Sequence?
+# In Python, a sequence is a data type that can store multiple, individual values
 
-#! List-----------
-#A list is the idea of multiple values but all contained in one named variable x=["a","b","c"]
-#! add items to a list:
+# ! List-----------
+# A list is the idea of multiple values but all contained in one named variable x=["a","b","c"]
+# ! add items to a list:
 # .append() add individual items, modifies the list
 # You can concatenate two lists together (" "+ " "). Concatenation produces a new List.
 # You can .extend() a list using elements from another list, modifies the list
 
-#! len()
+# ! len()
 # Get the number of elements in a sequence.
-#! del variable[]
-#delets an item
- 
-#! List Slicing
-#variable[0:2] --> slice from 0 to 2 not including 2, return new list
+# ! del variable[]
+# delets an item
 
-#! list.insert(3, item) 
+# ! List Slicing
+# variable[0:2] --> slice from 0 to 2 not including 2, return new list
+
+# ! list.insert(3, item)
 # - inserts item at specified location
-#! list.pop() 
+# ! list.pop()
 # - removes an item from the end of the list
-#! list.index(item) 
+# ! list.index(item)
 # - returns the index of the item in the list
-#! list.sort() 
+# ! list.sort()
 # - sorts the list, sorts the original list
-#! list.copy() 
+# ! list.copy()
 # - returns a new copy of the list
-#! list.clear() 
+# ! list.clear()
 # - removes all items from the list
+
+
+# !Matrix
+# matrix = [[1,2,3],
+#           [4,5,6],
+#           [7,8,9],
+#           [9,10,11]
+#         ]
+# access matrix:
+# print(matrix[0]) -- [1, 2, 3]
+# print(matrix[1][1]) -- 5
+
+# text = "hello"
+# index = 0
+# while len(text) > index:
+#     index -= 1
+#     print(text[index])
+
+# ! ----range---
+
